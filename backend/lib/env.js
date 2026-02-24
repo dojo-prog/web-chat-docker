26f4c3d9f16c46ba1f1c053f2f9deddb15ff875a
dojo <djdumaluan.prog@gmail.com>
2026-02-25 00:04:29 +0800

    added access & refresh token secret and node env

--- backend/lib/env.js
+++ backend/lib/env.js
@@ -3,6 +3,9 @@ import "dotenv/config";
 const ENV = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
 };
 
 export default ENV;
