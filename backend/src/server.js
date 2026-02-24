a9be5840de15781664782cfefd101b9699a6a25f
dojo <djdumaluan.prog@gmail.com>
2026-02-24 23:15:19 +0800

    registered auth router & global error handler middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,18 @@ import e from "express";
 import ENV from "../lib/env.js";
 import cookieParser from "cookie-parser";
 import connectDB from "../lib/db.js";
+import authRouter from "../routes/auth.route.js";
+import { errorHandler } from "../middlewares/error.middleware.js";
 
 const app = e();
 const PORT = ENV.PORT || 5000;
 
 app.use(e.json({ limit: "3mb" }));
 app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server connected to port ${PORT}`);
