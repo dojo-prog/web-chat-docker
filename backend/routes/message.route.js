81f04557963c6d83cb23a61d735b44493ae1cce5
dojo <djdumaluan.prog@gmail.com>
2026-02-25 20:39:49 +0800

    added protectRoute middleware to all endpoints + renamed userId param to receiverId for the sendMessage endpoint

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -5,12 +5,15 @@ import {
   getAllUsers,
   sendMessage,
 } from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = e.Router();
+
+router.use(protectRoute);
 
 router.get("/", getAllUsers);
 router.get("/contacts", getAllContacts);
 router.get("/:userId", getAllUserMessages);
-router.post("/:userId", sendMessage);
+router.post("/:receiverId", sendMessage);
 
 export default router;
