8b6b04130ce8f884494e458e5fdcd825362817a5
dojo <djdumaluan.prog@gmail.com>
2026-02-25 23:08:14 +0800

    added main route / main layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,17 @@ import { ToastContainer } from "react-toastify";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Route>
       </Routes>
       <ToastContainer />
     </>
