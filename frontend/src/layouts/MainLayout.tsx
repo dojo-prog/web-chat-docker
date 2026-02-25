2c05a5521c62525da0ab885068ebbe72cad23e5b
dojo <djdumaluan.prog@gmail.com>
2026-02-25 23:45:57 +0800

    added styling to main layout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="h-screen w-screen bg-linear-to-t from-blue-500 to-black flex items-center justify-center px-5">
+      <div className="max-w-4xl w-full h-[80vh] bg-slate-900 rounded-xl text-white">
+        <Outlet />
+      </div>
     </div>
   );
 };
