762fe3a0e682aaed6afef5e15e4f7567ad6efd0c
dojo <djdumaluan.prog@gmail.com>
2026-02-25 23:06:14 +0800

    initialized routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
